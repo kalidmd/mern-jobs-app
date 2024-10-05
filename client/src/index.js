9cd093223b6e2a2141a88fcd2ade21b1fed79a26
Kalid Mohammed <110621255+kalidmd@users.noreply.github.com>
2024-10-05 23:05:36 +0300

    /

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from "react-router-dom";
 import './index.css';
 import router from './router';
 
+const loading = (
+    <div className="loading">
+      <div className="loading-ring"></div>
+    </div>
+  )
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <RouterProvider router={router}/>
+    <Suspense fallback={loading}>
+        <RouterProvider router={router}/>
+    </Suspense>
 );
 
